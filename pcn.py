aa3e65e0f0416d978329663786959325c62f2732
Hiroki <yamasaki_hiroki_kyoto@yahoo.co.jp>
2018-01-07 00:06:54 +0800

    new commit

--- pcn.py
+++ pcn.py
@@ -4,9 +4,32 @@ import numpy as np
 # convert maps to patches
 # @maps: maps of current layer
 # @patches: patches to fill
-# @ksize: kernel size, [depth,height,width]
+# @ksize: kernel size, [height,width]
 def m2p(maps, patches, ksize):
+    _m_shape = maps.shape
+    _p_shape = patches.shape
+    assert len(_m_shape)==3
+    assert len(_p_shape)==3
+    assert len(ksize)==2
+    assert _m_shape[0]-ksize[0]+1==_p_shape[0]
+    assert _m_shape[1]-ksize[1]+1==_p_shape[1]
+    assert _m_shape[2]*ksize[0]*ksize[1]==_p_shape[2]
+    _kh = ksize[0]
+    _kw = ksize[1]
+    _kd = _m_shape[-1]
+    _h = _p_shape[0]
+    _w = _p_shape[1]
+    for _y in xrange(_h): # y pixel
+        for _x in xrange(_w): # x pixel
+            for _ky in xrange(_kh): # kernel y
+                for _kx in xrange(_kw): # kernel x
+                    for _kz in xrange(_kd): # kernel depth
+                        patches[_y,_x,_kd*(_ky*_kw+_kx)+_kz] = \
+                        maps[_y+_ky,_x+_kx,_kz]
 
+####### Pattern Connection Network #########
+# Train network layer by layer
+# Visualize them to show why it should work!
 
 class pcn(object):
     def __init__(
@@ -25,7 +48,11 @@ class pcn(object):
         if y_dim is None:
             y_dim = [6]
         if pattern_dims is None:
-            pattern_dims = [[3, 3, 32], [3, 3, 16], [3, 3, 8]]
+            pattern_dims = [
+                [3, 3, 32],
+                [3, 3, 16],
+                [3, 3, 8]
+            ]
 
         self.x = np.zeros(x_dim)
         self.y = np.zeros(y_dim)
@@ -46,10 +73,18 @@ class pcn(object):
             else:
                 pattern_dims[i].insert(0, pattern_dims[i-1][-1])
             # create pattern variables with random values
-            _pattern_len = pattern_dims[i][0]*pattern_dims[i][1]*pattern_dims[i][2]
+            _pattern_len = pattern_dims[i][0]*\
+                           pattern_dims[i][1]*\
+                           pattern_dims[i][2]
             _pattern_num = pattern_dims[i][-1]
             # patterns will be normalized to [0,1] using polarization function
-            self.patterns.append(np.random.uniform(0,1.0,[_pattern_num, _pattern_len]))
+            self.patterns.append(
+                np.random.uniform(
+                    0,
+                    1.0,
+                    [_pattern_num, _pattern_len]
+                )
+            )
             # only valid convolution is supported, recompute its map width and height
             if i==0:
                 _h = x_dim[0] - pattern_dims[i][1] + 1
@@ -57,10 +92,13 @@ class pcn(object):
             else:
                 _h = self.maps[-1].shape[0] - pattern_dims[i][1] + 1
                 _w = self.maps[-1].shape[1] - pattern_dims[i][2] + 1
-            self.maps.append(np.zeros([_h,_w,_pattern_num]))
             # pre-allocation for patches, to avoid possible allocation error
             # during network training or prediction
-            self.patches.append(np.zeros(_h,_w,))
+            _vec_len = _pattern_len
+            self.patches.append(np.zeros(_h, _w, _pattern_len))
+            # maps = patches * patterns
+            self.maps.append(np.zeros([_h, _w, _pattern_num]))
+            # pattern counter update
             _sum_of_patterns += _pattern_num
 
         # activation for every pattern unit(not the map of pattern,
